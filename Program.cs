afea063693e451ba332bb8aa040abc00a372eb32
Anthony Arbour <xtacmx564@hotmail.com>
2022-04-09 11:30:07 -0400

    Completed Find Pi to the Nth Digit function

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace Mathify
 
         static void Initialize()
         {
+
             Console.WriteLine("Welcome to Mathify! Please select your desired function");
             Console.WriteLine("1: Find Pi to the Nth Digit");
             Console.WriteLine("2: Find e to the Nth Digit");
@@ -25,16 +26,39 @@ namespace Mathify
             if (input == "1")
             {
                 Console.Clear();
-                PiRange();
-            }
-                
+                do
+                {
+                    CalculatePI();
+                    Console.WriteLine("Calculate again? (y/n)");
+                } while (Console.ReadLine() != "n");
 
+                
+            }
+
+           
 
         }
-        static float PiRange()
+        static void CalculatePI()
         {
+            double pi = Math.Atan(1) * 4;
+            Console.WriteLine("Enter the number of digits you want to find in Pi? (Max: 15)");
 
-            return 1;
+            string input = Console.ReadLine();
+            int digit;
+            if (int.TryParse(input, out digit))
+            {
+                try
+                {
+                    double result = Math.Round(pi, digit);
+                    Console.WriteLine("Pi to {0} digits is {1}.", digit, result);
+                }
+                catch
+                {
+                    Console.WriteLine("The max digits is 15! Try again.");
+                }
+                
+            }
+            
         }
     }
 }
