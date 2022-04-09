d7ee81ab3848f2ba525d8dc59973b70e26d0aa0b
Anthony <Anthony@DESKTOP-28680>
2022-04-09 10:24:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Mathify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
